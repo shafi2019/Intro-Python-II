21e06ffb10438e8ee5414aeb515db27b5cebaf11
Shafi Masoumi <masoumi2017@icould.com>
2020-07-16 16:13:39 -0700

    room and player classes created

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, description):
+      self.name = name 
+      self.description = description
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, room):
+      self.name = name 
+      self.age = room
